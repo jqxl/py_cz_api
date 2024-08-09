70e42e83059bac90fc8add0a7340e61abb0b0f6b
Anstyev <jqxl@ya.ru>
2024-08-09 18:25:22 +0300

    added Return helper class

--- examples/doc_info.py
+++ examples/doc_info.py
@@ -4,7 +4,7 @@ import pandas as pd
 essep = '01EB1AA50033B12D894A535821B96C26C0'
 certificate = py_cz_api.Certificate(essep)
 token = py_cz_api.Token.create_from_cert(certificate)
-api = py_cz_api.ApiExtended(token, py_cz_api.Pgs.ncp)
+api = py_cz_api.Api(token, py_cz_api.Pgs.ncp)
 
 doc = 'ON_NSCHFDOPPRMARK_2be241ee8c99aae445c9917226eb52b81df_2BM-9729308375-771401001-202311220641472597668_20240807_b5f64dcf-eb95-4461-b19c-f748952fb16a'
 ans = api.doc_info(doc, body=True)
--- py_cz_api/buisness/mark_returns.py
+++ py_cz_api/buisness/mark_returns.py
@@ -1,3 +1,5 @@
+import pandas as pd
+
 from .pd_cz import ApiExtended
 
 needs_cols = ['status',
@@ -5,3 +7,22 @@ needs_cols = ['status',
               'child',
               'producerInn']
 
+class Returns:
+    whom_inn:str
+    us_inn:str
+    input_mark_list:list
+    input_mark_df:pd.DataFrame
+
+    taboo_mark_list:list
+
+    def __init__(self,
+                 mark_list:list,
+                 whom_inn:str,
+                 us_inn:str):
+        self.input_mark_list = mark_list
+        self.input_mark_df = pd.DataFrame({'input_mark_list':mark_list})
+        self.whom_inn = whom_inn
+        self.us_inn = us_inn
+
+    def foo():
+        pass
